c799ebecd1a1d0d5a25732bf2341eb2c5fbde636
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-01-12 00:18:00 +0200

    Fix the not-reproducible bug
    <all_urls> is required as a permission for background.js and manifest.json was not being reloaded as I disabled-reenabled the extension during dev

--- background.js
+++ background.js
@@ -6,6 +6,8 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
         // Set how many top-level comments to load at a time. 100 seems to be the enforced max
         url.searchParams.set('page[count]', 1000)
 
+        console.log('Intercepted', details.url)
+
         return { redirectUrl: url.toString() }
     },
     {
