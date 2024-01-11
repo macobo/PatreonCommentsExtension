bfc239ec6ee7457abf7b0b018f6213881ad08e2f
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-01-11 23:28:13 +0200

    Make background.js matching less restrictive in an attempt to fix non-reproducible bug

--- background.js
+++ background.js
@@ -3,15 +3,13 @@
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
         const url = new URL(details.url)
 
-        url.searchParams.set(
-            'page[count]',
-            1000
-        )
+        // Set how many top-level comments to load at a time. 100 seems to be the enforced max
+        url.searchParams.set('page[count]', 1000)
 
         return { redirectUrl: url.toString() }
     },
     {
-        urls: ["https://www.patreon.com/api/posts/*/comments*"]
+        urls: ["*://www.patreon.com/api/posts/*"]
     },
     ["blocking"]
 );
